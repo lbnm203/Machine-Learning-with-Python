a2cb71345c63af23cde60d7c2d408de89e662107
lbnm203 <lebanhatminh1303@gmail.com>
2025-02-24 11:23:53 +0700

    update: mnist classifier

---
+++ services/mnist_classfier/utils/demo_st.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import mlflow
+import mlflow.sklearn
+import joblib
+
+from PIL import Image
+from streamlit_drawable_canvas import st_canvas
+
+
+def preprocess_canvas_image(canvas_result):
+    if canvas_result.image_data is not None:
+        img = Image.fromarray(
+            canvas_result.image_data[:, :, 0].astype(np.uint8))
+        # Resize và chuyển thành grayscale
+        img = img.resize((28, 28)).convert("L")
+        img = np.array(img, dtype=np.float32) / 255.0  # Chuẩn hóa về [0, 1]
+        return img.reshape(1, -1)  # Chuyển thành vector 1D
+    return None
+
+
+@st.cache(allow_output_mutation=True)
+def load_model(path):
+    try:
+        return joblib.load(path)
+    except FileNotFoundError:
+        st.error(f"⚠️ Không tìm thấy mô hình tại `{path}`")
+        st.stop()
+
+
+def demo_app():
+    st.write("## Demo APP")
+    models = {
+        "SVM Linear": "./services/mnist_classfier/models/svm_mnist_linear.joblib",
+        "SVM Poly": "./services/mnist_classfier/models/svm_mnist_poly.joblib",
+        "SVM Sigmoid": "./services/mnist_classfier/models/svm_mnist_sigmoid.joblib",
+        "SVM RBF": "./services/mnist_classfier/models//svm_mnist_rbf.joblib",
+    }
+
+    # Lấy tên mô hình từ session_state
+    model_names = [model["name"]
+                   for model in st.session_state.get("models", [])]
+
+    if "models" not in st.session_state:
+        st.session_state["models"] = []
+
+    # 📌 Chọn mô hình
+    model_option = st.selectbox(
+        "🔍 Chọn mô hình:", list(models.keys()) + model_names)
+
+    # Nếu chọn mô hình đã được huấn luyện và lưu trong session_state
+    if model_option in model_names:
+        model = next(
+            model for model in st.session_state["models"] if model["name"] == model_option)["model"]
+    else:
+        # Nếu chọn mô hình có sẵn (các mô hình đã được huấn luyện và lưu trữ dưới dạng file)
+        model = load_model(models[model_option])
+        st.success(f"✅ Đã tải mô hình: {model_option}")
+
+    # ✍️ Vẽ số
+    with st.container(border=True):
+        col1, col2 = st.columns(2)
+        with col1:
+            canvas_result = st_canvas(
+                fill_color="black",
+                stroke_width=10,
+                stroke_color="white",
+                background_color="black",
+                height=150,
+                width=150,
+                drawing_mode="freedraw",
+                key="digit_canvas"
+            )
+
+        if st.button("Dự đoán số"):
+            with col2:
+                img = preprocess_canvas_image(canvas_result)
+
+                if img is not None:
+                    # Hiển thị ảnh sau xử lý
+                    st.image(Image.fromarray((img.reshape(28, 28) *
+                                              255).astype(np.uint8)), caption="Ảnh sau xử lý", width=100)
+
+                    # Dự đoán
+                    prediction = model.predict(img)
+                    st.subheader(f"🔢 Dự đoán: {prediction[0]}")
+                else:
+                    st.error("⚠️ Hãy vẽ một số trước khi bấm Dự đoán!")
---
+++ services/mnist_classfier/utils/training.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import mlflow
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+def train_process(X, y):
+    st.write("## ⚙️ Quá trình huấn luyện")
+
+    total_samples = X.shape[0]
+
+    # Thanh kéo chọn số lượng ảnh để train
+    num_samples = st.slider("Chọn số lượng ảnh để train:",
+                            1000, total_samples, 10000)
+
+    # Thanh kéo chọn tỷ lệ Train/Test
+    test_size = st.slider("Chọn tỷ lệ test:", 0.1, 0.5, 0.2)
+
+    X_selected, y_selected = X[:num_samples], y[:num_samples]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_selected, y_selected, test_size=test_size, random_state=42)
+
+    # Lưu vào session_state để sử dụng sau
+    st.session_state["X_train"] = X_train
+    st.session_state["y_train"] = y_train
+    st.session_state["X_test"] = X_test
+    st.session_state["y_test"] = y_test
+
+    if "X_train" in st.session_state:
+        X_train = st.session_state["X_train"]
+        # st.write(X_train.dtype)
+        y_train = st.session_state["y_train"]
+        X_test = st.session_state["X_test"]
+        y_test = st.session_state["y_test"]
+    else:
+        st.error("⚠️ Chưa có dữ liệu! Hãy chia dữ liệu trước.")
+        return
+
+    st.write(f'Training: {X_train.shape[0]} - Testing: {y_test.shape[0]}')
+
+    X_train = X_train.reshape(-1, 28 * 28) / 255.0
+    X_test = X_test.reshape(-1, 28 * 28) / 255.0
+
+    st.write("---")
+
+    # 📌 **Chọn mô hình**
+    model_choice = st.selectbox("Chọn mô hình:", ["Decision Tree", "SVM"])
+
+    if model_choice == "Decision Tree":
+        st.markdown("""
+        - **Tham số mô hình:**  
+            - **max_depth**: Độ sâu tối đa của cây.  
+                - **Giá trị nhỏ**: Tránh overfitting nhưng có thể underfitting.  
+                - **Giá trị lớn**: Cây có thể học tốt hơn nhưng dễ bị overfitting.  
+        """)
+
+        max_depth = st.slider("max_depth", 1, 20, 5)
+        model = DecisionTreeClassifier(max_depth=max_depth)
+
+    elif model_choice == "SVM":
+        st.markdown("""
+        - **Tham số cần chọn:**  
+            - **C (Regularization)**: Hệ số điều chỉnh độ phạt lỗi.  
+                - **C nhỏ**: Mô hình đơn giản hơn, chấp nhận nhiều lỗi hơn.  
+                - **C lớn**: Mô hình cố gắng phân loại chính xác mọi điểm, nhưng dễ bị overfitting.  
+            - **Kernel**: Hàm kernel trick giúp phân tách dữ liệu phi tuyến tính bằng cách ánh xạ dữ 
+                liệu vào không gian có nhiều chiều hơn.
+                - Linear: Mô hình dùng siêu phẳng tuyến tính để phân lớp.  
+                - RBF: Kernel Gaussian Radial Basis Function giúp phân tách dữ liệu phi tuyến tính tốt hơn.  
+                - Poly: Sử dụng đa thức bậc cao để phân lớp, phù hợp với dữ liệu có mối quan hệ phức tạp.  
+                - Sigmoid: Mô phỏng hàm kích hoạt của mạng nơ-ron.
+ 
+        """)
+        C = st.slider("C (Regularization)", 0.1, 10.0, 1.0)
+        kernel = st.selectbox("Kernel", ["linear", "rbf", "poly", "sigmoid"])
+        model = SVC(C=C, kernel=kernel)
+
+    if st.button("Huấn luyện mô hình"):
+        with mlflow.start_run():
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            acc = accuracy_score(y_test, y_pred)
+            st.success(f"✅ Độ chính xác: {acc:.4f}")
+
+            mlflow.log_param("model", model_choice)
+            if model_choice == "Decision Tree":
+                mlflow.log_param("max_depth", max_depth)
+            elif model_choice == "SVM":
+                mlflow.log_param("C", C)
+                mlflow.log_param("kernel", kernel)
+
+            mlflow.log_metric("accuracy", acc)
+            mlflow.sklearn.log_model(model, model_choice.lower())
+
+            st.success("📌 Mô hình đã được lưu trên MLflow!")
+
+        if model_choice == "Decision Tree":
+            depths = range(1, 21)
+            accuracies = []
+            for depth in depths:
+                model = DecisionTreeClassifier(max_depth=depth)
+                model.fit(X_train, y_train)
+                y_temp_pred = model.predict(X_test)
+                temp_acc = accuracy_score(y_test, y_temp_pred)
+                accuracies.append(temp_acc)
+
+            accuracy_df = pd.DataFrame(
+                {"Độ sâu": depths, "Độ chính xác": accuracies})
+            st.line_chart(accuracy_df.set_index("Độ sâu"))
+
+        # st.success(f"✅ Độ chính xác: {temp_acc:.4f}")
+
+        # model.fit(X_train, y_train)
+        # y_pred = model.predict(X_test)
+        # acc = accuracy_score(y_test, y_pred)
+        # st.success(f"✅ Độ chính xác: {acc:.4f}")
+
+        # Lưu mô hình vào session_state dưới dạng danh sách nếu chưa có
+        if "models" not in st.session_state:
+            st.session_state["models"] = []
+
+        # Tạo tên mô hình dựa trên lựa chọn mô hình và kernel
+        model_name = model_choice.lower().replace(" ", "_")
+        if model_choice == "SVM":
+            model_name += f"_{kernel}"
+
+        # Kiểm tra nếu tên mô hình đã tồn tại trong session_state
+        existing_model = next(
+            (item for item in st.session_state["models"] if item["name"] == model_name), None)
+
+        if existing_model:
+            # Tạo tên mới với số đếm phía sau
+            count = 1
+            new_model_name = f"{model_name}_{count}"
+
+            # Kiểm tra tên mới chưa tồn tại
+            while any(item["name"] == new_model_name for item in st.session_state["models"]):
+                count += 1
+                new_model_name = f"{model_name}_{count}"
+
+            # Sử dụng tên mới đã tạo
+            model_name = new_model_name
+            # st.warning(f"⚠️ Mô hình được lưu với tên là: {model_name}")
+
+        # # Lưu mô hình vào danh sách với tên mô hình cụ thể
+        st.session_state["models"].append({"name": model_name, "model": model})
+        # st.write(f"🔹 Mô hình đã được lưu với tên: {model_name}")
+        # st.write(
+        #     f"Tổng số mô hình hiện tại: {len(st.session_state['models'])}")
+
+        # # In tên các mô hình đã lưu
+        # st.write("📋 Danh sách các mô hình đã lưu:")
+        model_names = [model["name"] for model in st.session_state["models"]]
+        # # Hiển thị tên các mô hình trong một d
+        # st.write(", ".join(model_names))
