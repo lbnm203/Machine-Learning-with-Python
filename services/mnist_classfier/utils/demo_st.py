13b518e4ece27c027465bfec87d0db75c70d4f38
lbnm203 <lebanhatminh1303@gmail.com>
2025-02-24 11:34:43 +0700

    upd

--- services/mnist_classfier/utils/demo_st.py
+++ services/mnist_classfier/utils/demo_st.py
@@ -20,7 +20,7 @@ def preprocess_canvas_image(canvas_result):
     return None
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data(allow_output_mutation=True)
 def load_model(path):
     try:
         return joblib.load(path)
